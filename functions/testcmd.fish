a6e00ff294e1fe8b2deed777177c619d5bf9bb6e
kevinkenan <kevin@kevinkenan.com>
2021-06-14 05:53:01 -0700

    clean up testcmd

--- functions/testcmd.fish
+++ functions/testcmd.fish
@@ -48,28 +48,6 @@ function testcmd
 		echo That which does not kill us makes us stronger.
 	end
 
-	# function _testcmd:test:this
-	# 	set -a opts (fish_opt -s A -l arg1 -r --long-only)
-	# 	set -a opts (fish_opt -s B -l arg2 -r --long-only)
-	# 	set -a opts (fish_opt -s a -r)
-	# 	set -a opts (fish_opt -s n -l nothing)
-	# 	argparse -n "$_cmdpath" $opts -- $_args; or return
-
-	# 	_cmd_register --action \
-	# 		--help_text "Print an inspirational message." \
-	# 		--arg_list "--arg1 ARG1 --arg2 ARG2" \
-	# 		--arg_help "
-	# 		  ARG1   This is the first argument that is ignored.
-	# 		  ARG2   The second argument is just as ignored as the first." \
-	# 		--opt_help "
-	# 		  -a VALUE        Does absolutely nothing of VALUE.
-	# 		  -n, --nothing   Does nothing.
-	# 		  -t              Does even more of nothing."
-	# 	or return
-
-	# 	echo That which does not kill us makes us stronger.
-	# end
-
 	function _testcmd:test:that
 		_cmd_register  --action --no_opts \
 			--help_text "Print a despairing message."
